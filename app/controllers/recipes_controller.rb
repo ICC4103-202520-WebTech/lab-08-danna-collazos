50511e61c4e84222e0e2669d2169d5310f1fce5c
Danna Collazos <ddcollazos@miuandes.cl>
2025-10-20 17:47:23 -0300

    add authorization

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  load_and_authorize_resource
   before_action :set_recipe, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, only: %i[ create edit update destroy new ]
 
